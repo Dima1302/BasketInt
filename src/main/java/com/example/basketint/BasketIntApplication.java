fe8dc229a3153aeddac16edb31d104714aed9a52
Dmitriy <Dimitriy303@yandex.ru>
2022-08-21 22:23:24 +0900

    Initial commit

---
+++ src/main/java/com/example/basketint/BasketIntApplication.java
@@ -0,0 +1,13 @@
+package com.example.basketint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketIntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basketint/BasketIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basketint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
